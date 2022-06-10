496bd170006ba04c03b1a5da6e481a15b76de548
rashidkhan <rashidkhanbd57@gmail.com>
2022-06-10 08:04:31 +0600

    banner added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom';
+import './Banner.css'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const Banner = () => {
   return (
     <div>
-      <div className="container mx-auto px-2 ">
-
-        <div class="hero h-1/2 bg-base-200">
-          <div class="hero-content flex-col lg:flex-row-reverse">
-            <img src="https://i.ibb.co/9rj1Sk7/MG-5424.jpg" class="max-w-sm rounded-full shadow-2xl" alt='banner'/>
+      <div className="container mx-auto ">
+        <div class="bg-slate-400"  id="banner">
+          <div class="md:flex items-center justify-around  px-4 flex-row-reverse" id="bannerContent">
+            <div className="text-center md:mt-10">
+              <img src="https://i.ibb.co/9rj1Sk7/MG-5424.jpg" class=" rounded-full shadow-2xl m-auto " alt='banner' style={{width:'300px'}}/>
+            </div>
             <div>
-              <h1 class="text-5xl font-bold">Box Office News!</h1>
-              <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button class="btn btn-primary">Get Started</button>
+             <div className="">
+             <h1 class="text-5xl font-bold text-sky-600">Hi! I am Rashid Khan ! </h1>
+              <p class="py-6 text-2xl text-slate-800">I am a font-end developer and I can design and develop any web site.<br></br> Click the resume button and see my resume</p>
+              <a target={'_blank'} href='https://drive.google.com/file/d/1uDjdWBJuly4Uu66tm0V9pivfcjZQhf6Y/view' className="bg-blue-600 hover:bg-blue-800 p-2 px-6 text-white text-xl uppercase font-serif rounded-bl-xl rounded-tr-xl">Resume</a>
+             </div>
+              <div className="">
+            <ul>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+            </ul>
+          </div>
             </div>
           </div>
+          
         </div>
       </div>
     </div>
